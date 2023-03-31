590741684ae086e4e9dc44b2cc2bdf9d37f264da
Noah Poling <xmosphere@gmail.com>
2023-03-31 17:49:37 -0400

    Removed Reference to vite.svg that was causing error in deployment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,6 @@ function Footer(){
     return (
         <footer>
             <h5>Powered by Vite and React</h5>
-            <a href="https://vitejs.dev" target="_blank">
-                <img src="/vite.svg" className="logo" alt="Vite logo" />
-            </a>
             <a href="https://reactjs.org" target="_blank">
                 <img src={reactLogo} className="logo react" alt="React logo" />
             </a>
