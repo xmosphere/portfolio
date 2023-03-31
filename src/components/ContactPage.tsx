344b20fbdd9a79e1b59b15bfbcd2b8a6f330f23c
Noah Poling <xmosphere@gmail.com>
2023-03-31 17:58:26 -0400

    Removed Mail button that was causing deploy error and moved it to contact page

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import {theme, TitleText, SubTitleText, Text, ImgLinkButton, ThemedButton} from '../assets/theme'
-import Link from './MailButton'
+import { Link } from "react-router-dom";
 
 const Page = styled.div`
         height: 90vh;
@@ -19,10 +19,19 @@ const ContactPage = () => {
         <Page>
             <FlexDiv>
                 <TitleText>E-Mail Me</TitleText>
-                <Link label={"Mail Me"} mailto={"mailto:noahspoling@gmail.com"}></Link>
-            </FlexDiv>
+                <ThemedButton>
+                    <Link
+                        to='#'
+                        onClick={(e) => {
+                        window.location.href = "mailto:noahspoling@gmail.com";
+                        e.preventDefault();
+                        }}>
+                    <SubTitleText>E-Mail Me</SubTitleText>
+                    </Link>
+            </ThemedButton>
+                </FlexDiv>
             <FlexDiv>
-                <TitleText>Connect on LinkedIn</TitleText>
+                <SubTitleText>Connect on LinkedIn</SubTitleText>
                 <a href="https://www.linkedin.com/in/noah-poling-4abbba260/">
                         <ImgLinkButton style={{borderRadius: "50%"}}><img src="src/assets/linkedin.png" alt="LinkedIn" height="50px" width="50px"/>
                         </ImgLinkButton>
